8a576acf622e8bff6ea5a387ea774403740f1ba0
sho <miomini3715@gmail.com>
2024-01-05 23:57:30 +0900

    処理の修正

---
+++ app/profile/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const ProfilePage: React.FC = () => {
+  return (
+    <div className='flex flex-col items-center justify-center min-h-screen py-2'>
+      <h1 className='text-4xl font-bold mb-4'>プロフィールページ</h1>
+      <p className='text-lg text-center'>プロフィールページです。</p>
+    </div>
+  )
+}
+
+export default ProfilePage
